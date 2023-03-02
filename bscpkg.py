6d20367b3efae3c1f99ba6cc88bebb19396ef596
Macgaiver <126487835+macgaiver11996@users.noreply.github.com>
2023-03-02 11:31:09 +0600

    Add files via upload

---
+++ bscpkg.py
@@ -0,0 +1,60 @@
+# This tool make by M4C641VER
+import os
+
+os.system('clear')
+
+logo = """\033[1;32m
+
+______       ______ _         
+| ___ \      | ___ \ |        
+| |_/ /______| |_/ / | ____ _ 
+| ___ \______|  __/| |/ / _` |
+| |_/ /      | |   |   < (_| |
+\____/       \_|   |_|\_\__, |
+                         __/ |
+                        |___/ 
+"""
+
+print (logo)
+print ("\x1b[31mCreator : Macgaiver")
+print ("Github  : macgaiver11996")
+print ("Contact : fb.com/macgaiver.parvez\n")
+
+os.system ("apt update -y")
+os.system ("apt upgrade -y")
+os.system ("termux-setup-storage -y")
+os.system ("apt install git -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install nano -y")
+os.system ("apt install fish -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cmatrix -y")
+os.system ("apt install toilet -y")
+os.system ("apt install ruby -y")
+os.system ("apt install php -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install proof -y")
+os.system ("apt install proot -y")
+os.system ("apt install lolcat -y")
+os.system ("pip install pip -y")
+os.system ("pip install future -y")
+os.system ("pip install pip2 -y")
+os.system ("pip2 install requests -y")
+os.system ("pip2 install machanize -y")
+os.system ("pip2 install requirements -y")
+
+
+logo = """\033[1;32m
+
+ _    _      _                          
+| |  | |    | |                         
+| |  | | ___| | ___ ___  _ __ ___   ___ 
+| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \
+\  /\  /  __/ | (_| (_) | | | | | |  __/
+ \/  \/ \___|_|\___\___/|_| |_| |_|\___|
+                                        
+                                        
+"""
+print (logo)
